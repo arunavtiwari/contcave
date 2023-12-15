b20edfd154381026d0e5072f03010715a1141a29
Harshita <harshitabetala7@gmail.com>
2023-12-15 18:53:43 +0530

    footer added

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({
           <Navbar currentUser={currentUser} />
         </ClientOnly>
         <div className="pb-20 pt-28">{children}</div>
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
