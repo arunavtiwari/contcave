f603ef4ba349357b61b3e0a1956d9bf987229afd
Harshita <harshitabetala7@gmail.com>
2024-10-14 19:26:42 +0530

    Update features cards UI

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -3,46 +3,28 @@ import { Feature } from "@/types/feature";
 const featuresData: Feature[] = [
   {
     id: 1,
-    icon: "/images/icon/find.svg",
+    icon: "/images/features/pexels-ron-lach-8857043.jpg",
     title: "Find Your Perfect Space ",
     description:
       "Browse studios tailored to your needs – from intimate podcast rooms to expansive film sets. Access cutting-edge equipment and expert talent, all at your fingertips.",
+    button_text: "Find Now"
   },
-  // {
-  //   id: 2,
-  //   icon: "/images/icon/icon-02.svg",
-  //   title: "Unlock Your Potential",
-  //   description:
-  //     "",
-  // },
   {
-    id: 3,
-    icon: "/images/icon/connect.svg",
+    id: 2,
+    icon: "/images/features/pexels-cottonbro-6764782.jpg",
     title: "Connect and Collaborate",
     description:
-      "Forge meaningful connections with fellow creators and collaborators. Join a Contcave's  vibrant community where ideas ignite and stories come alive. Share your passion, connect with collaborators, and elevate your work to new heights.",
+      "Forge meaningful connections with fellow creators and collaborators. Join a ContCave's  vibrant community where ideas ignite and stories come alive. Share your passion, connect with collaborators, and elevate your work to new heights.",
+    button_text: "Connect Now"
   },
   {
-    id: 4,
-    icon: "/images/icon/brand.svg",
+    id: 3,
+    icon: "/images/features/pexels-cottonbro-3779072.jpg",
     title: "Your Vision, Our Platform",
     description:
-      "Focus on what matters – your creative vision – by leaving the rest to us.Reach a wider audience and share your stories with the world.",
+      "Focus on what matters – your creative vision – by leaving the rest to us. Reach a wider audience and share your stories with the world.",
+    button_text: "Fulfil your Vision"
   },
-  // {
-  //   id: 5,
-  //   icon: "/images/icon/icon-05.svg",
-  //   title: "Break down barriers",
-  //   description:
-  //     "Access the resources you need to bring your ideas to life",
-  // },
-  // {
-  //   id: 6,
-  //   icon: "/images/icon/icon-06.svg",
-  //   title: "Amplify your voice",
-  //   description:
-  //     "",
-  // },
 ];
 
 export default featuresData;
