3c8f433b6b2d45f459ca8a99a2359fe534d5c72c
Sam8239 <samanta.shubham99@gmail.com>
2024-11-30 19:37:47 +0530

    Updated

--- components/FAQ/index.tsx
+++ components/FAQ/index.tsx
@@ -14,7 +14,7 @@ const FAQ = () => {
 
   return (
     <>
-      {/* <!-- ===== FAQ Start ===== --> */}
+      {/* <!-- ===== FAQ Start ==== --> */}
       <section className="overflow-hidden pb-20 lg:pb-25 xl:pb-30">
         <div className="relative mx-auto max-w-c-1235 px-4 md:px-8 xl:px-0">
           <div className="absolute -bottom-16 -z-1 h-full w-full">
