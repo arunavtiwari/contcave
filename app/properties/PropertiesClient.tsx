78fbfe82e92f2b00162f28a0d310f0ca89cb8ae2
Sam8239 <samanta.shubham99@gmail.com>
2024-12-04 15:11:10 +0530

    Added Cookies Consent

--- app/properties/PropertiesClient.tsx
+++ app/properties/PropertiesClient.tsx
@@ -65,25 +65,27 @@ function PropertiesClient({ listings, currentUser }: Props) {
     [router]
   );
   return (
-    <Container>
-      <Heading title="Properties" subtitle="List of your properties" />
-      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
-        {listings.map((listing: any) => (
-          <ListingCard
-            key={listing.id}
-            data={listing}
-            actionId={listing.id}
-            onAction={onDelete}
-            onDelete={onDelete}
-            onEdit={onEdit}
-            onChat={onChat}
-            disabled={deletingId === listing.id}
-            actionLabel="Delete property"
-            currentUser={currentUser}
-          />
-        ))}
-      </div>
-    </Container>
+    <div className="mt-5">
+      <Container>
+        <Heading title="My Properties" subtitle="Efficiently Manage, Update, and Showcase Your Listings with Ease." />
+        <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
+          {listings.map((listing: any) => (
+            <ListingCard
+              key={listing.id}
+              data={listing}
+              actionId={listing.id}
+              onAction={onDelete}
+              onDelete={onDelete}
+              onEdit={onEdit}
+              onChat={onChat}
+              disabled={deletingId === listing.id}
+              actionLabel="Delete property"
+              currentUser={currentUser}
+            />
+          ))}
+        </div>
+      </Container>
+    </div>
   );
 }
 
