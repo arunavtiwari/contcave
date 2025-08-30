70babbf826bae39fe425833d210e28db2eb366cf
Shubham Samanta <samanta.shubham99@gmail.com>
2025-08-30 19:04:53 +0530

    Updated

--- app/actions/getTransaction.ts
+++ app/actions/getTransaction.ts
@@ -5,7 +5,7 @@ type Args = { tid: string };
 export default async function getTransaction({ tid }: Args) {
   if (!tid) return null;
   return prisma.transaction.findFirst({
-    where: { cfOrderId: tid },
+    where: { cfTxnRef: tid },
     include: {
       reservation: { include: { listing: true } },
       listing: true,
