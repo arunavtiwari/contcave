5bc948510352ae534d4495326fdef5ece3df16ec
Sam8239 <samanta.shubham99@gmail.com>
2026-04-09 16:40:01 +0530

    Fixed Build Errors

---
+++ components/navbar/NavbarWrapper.tsx
@@ -0,0 +1,16 @@
+import { Suspense } from "react";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import Navbar from "@/components/navbar/Navbar";
+
+async function NavbarLoader() {
+    const currentUser = await getCurrentUser();
+    return <Navbar currentUser={currentUser} />;
+}
+
+export default function NavbarWrapper() {
+    return (
+        <Suspense fallback={<Navbar currentUser={null} />}>
+            <NavbarLoader />
+        </Suspense>
+    );
+}
