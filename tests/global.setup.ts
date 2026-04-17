99d14cf4affa7375ef72c338da7dc556465002db
Sam8239 <samanta.shubham99@gmail.com>
2026-04-17 16:07:31 +0530

    Remove Tests

--- tests/global.setup.ts
+++
@@ -1,9 +0,0 @@
-import { execSync } from "node:child_process";
-
-async function globalSetup() {
-  execSync("npm run seed:test-owner", {
-    stdio: "inherit",
-  });
-}
-
-export default globalSetup;
