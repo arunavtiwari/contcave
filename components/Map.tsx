cc17f4b961b73a51c516a8b637cbd112a078749f
Sam8239 <samanta.shubham99@gmail.com>
2026-04-24 14:17:14 +0530

    Updated Map

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,63 +3,61 @@
 import "leaflet/dist/leaflet.css";
 
 import L from "leaflet";
-import { useEffect } from "react";
-import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
-
-const customIcon = L.icon({
-  iconUrl: "/images/icons/marker.png",
-  iconRetinaUrl: "/images/icons/marker.png",
-  iconSize: [38, 38],
-  iconAnchor: [19, 38],
-  shadowUrl: "",
-});
-
-function FlyToMarker({ center }: { center: L.LatLngExpression }) {
-  const map = useMap();
-  useEffect(() => {
-    map.flyTo(center, 14, {
-      animate: true,
-      duration: 1.5,
-    });
-  }, [center, map]);
-  return null;
-}
+import { useMemo } from "react";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 
 type Props = {
   center?: [number, number];
-  locationValue?: string;
 };
 
-function Map({ center }: Props) {
-  const isValidCenter =
-    Array.isArray(center) &&
-    center.length >= 2 &&
-    Number.isFinite(center[0]) &&
-    Number.isFinite(center[1]);
+const INDIA_CENTER: [number, number] = [20.5937, 78.9629];
 
-  const mapCenter: L.LatLngExpression = isValidCenter
-    ? [center[0], center[1]]
-    : [20.5937, 78.9629];
+const TILE_URL =
+  "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png";
+
+function Map({ center }: Props) {
+  const isValidCenter = useMemo(() => {
+    return (
+      Array.isArray(center) &&
+      center.length === 2 &&
+      Number.isFinite(center[0]) &&
+      Number.isFinite(center[1])
+    );
+  }, [center]);
+
+  const mapCenter = useMemo<[number, number]>(() => {
+    if (!isValidCenter || !center) return INDIA_CENTER;
+    return [center[0], center[1]];
+  }, [center, isValidCenter]);
+
+  const customIcon = useMemo(() => {
+    return L.icon({
+      iconUrl: "/images/icons/marker.png",
+      iconRetinaUrl: "/images/icons/marker.png",
+      iconSize: [38, 38],
+      iconAnchor: [19, 38],
+    });
+  }, []);
 
   return (
     <MapContainer
       center={mapCenter}
       zoom={isValidCenter ? 14 : 4}
-      scrollWheelZoom={false}
+      minZoom={3}
+      maxZoom={19}
+      scrollWheelZoom={true}
+      zoomControl={false}
       attributionControl={false}
-      preferCanvas={true}
-      className="h-[35vh] w-full rounded-xl z-0"
+      preferCanvas
+      className="z-0 h-[35vh] w-full rounded-xl"
     >
       <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        url={TILE_URL}
+        maxZoom={19}
+        detectRetina
       />
 
-      {isValidCenter && (
-        <>
-          <FlyToMarker center={mapCenter} />
-          <Marker position={mapCenter} icon={customIcon} />
-        </>
-      )}
+      {isValidCenter && <Marker position={mapCenter} icon={customIcon} />}
     </MapContainer>
   );
 }
