ed1dcf6ea2f72725d297fc5cb94db0f5a2e3fb62
Harshita <harshitabetala7@gmail.com>
2026-06-27 15:22:01 +0530

    minor syntax fix

--- components/listing/SetSelector.tsx
+++ components/listing/SetSelector.tsx
@@ -10,9 +10,6 @@ import { IoInformationCircleOutline } from "react-icons/io5";
 import type { Swiper as SwiperClass } from "swiper";
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-import { IoInformationCircleOutline } from "react-icons/io5";
-
 import Checkbox from "@/components/inputs/Checkbox";
 import SetDetailModal from "@/components/listing/SetDetailModal";
 import Heading from "@/components/ui/Heading";
